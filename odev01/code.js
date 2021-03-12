8463aa9bb5cc836d1e0e78d07ffb7a0293895602
ipekaltinok <1030516770@erciyes.edu.tr>
2021-03-12 18:56:23 +0300

    Odev01_son
    odev 1 i yuklememiştim,bu şekilde yukluyorum.gec oldu guc olmasın :D

--- odev01/code.js
+++ odev01/code.js
@@ -1,66 +1,67 @@
-const quizzes = [
-    {
-        question: "JavaScript hangi tipte dildir?",
-        answer_0: "Güçlü ve statik yazımlı",
-        answer_1: "Güçlü ve dinamik yazımlı",
-        answer_2: "Zayıf ve statik yazımlı",
-        answer_3: "Zayıf ve dinamik yazımlı",
-        indexOfRightAnswer: 3
-    },
-    {
-        question: "JavaScript dilinde aşağıda verilen kodun çıktısı nedir? \n\n+(!![]+!![]+!![]+!![]+[]+(!![]+!![]))",
-        answer_0: "Derleme hatası",
-        answer_1: "Çalışma zamanı hatası",
-        answer_2: "42",
-        answer_3: "'42'",
-        indexOfRightAnswer: 2
-    },
-    {
-        question: "JavaScript dilinde false + true ifadesinin çıktısı nedir?",
-        answer_0: "false",
-        answer_1: "true",
-        answer_2: "'falsetrue'",
-        answer_3: "1",
-        indexOfRightAnswer: 3
-    }
+
+
+let tahmin = 2;
+let x;
+let html;
+let html1;
+let html2;
+
+const resimler = [
+    "kedi.png",
+    "kopek.png",
+    "kopek.png",
 ];
 
-let latestIndex;
-const newQuiz = () => {
-    let index = Math.floor(Math.random()*quizzes.length);
-	//Yeni uretilen indeks eski indeks ile ayni ise bir sonraki soruya gecer. 
-    if(latestIndex === index) {
-        index = (index+1)%quizzes.length;
+const yeniOyun0 = () => {
+if(tahmin!==0){
+    let index = Math.floor(Math.random()*resimler.length);
+    if(x === index){
+        index = (index+1)%resimler.length;
     }
-    const quiz = quizzes[index];
-    latestIndex = index;
-    displayQuiz(quiz)
-}
-const displayQuiz = (quiz) => {
-    const { question, answer_0,answer_1,answer_2,answer_3, indexOfRightAnswer } = quiz; //esdegeri quiz.question, quiz.answer_0 ...
-	
-	/*quizDivId isimli div icerigini degistirmek icin html uretiyoruz. Ilk olarak soruya ait html uretilir ardindan cevaplar. 
-	Cevaplarin yapisi biraz daha karmasik ve tekrar eden bir yapi oldugu icin bir fonksiyon icinde uretmeyi tercih ettik. */
-    let html = "<p class='question'>Soru: "+question+"</p>\n";
-    html += getAnswerTag("A:", answer_0,indexOfRightAnswer===0);
-    html += getAnswerTag("B:", answer_1,indexOfRightAnswer===1);
-    html += getAnswerTag("C:", answer_2,indexOfRightAnswer===2);
-    html += getAnswerTag("D:", answer_3,indexOfRightAnswer===3);
-    const quizDiv = document.getElementById("quizDivId");
-    quizDiv.innerHTML = html;
-
-}
-
-const getAnswerTag = (prefix, answer, isCorrect) => {
-    let onclick;
-    if(isCorrect){
-        onclick = "alert('Doğru Cevap'); newQuiz();"
-    } else {
-        onclick = "alert('Yanlış Cevap');"
+    const image = resimler[index];
+    x = index;
+    html = document.getElementById("img0").src=image; 
+    if(resimler[0] === image){
+        document.getElementById("alanId").innerHTML="Kazandın!!! Tebrik ederiz :) Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
     }
-
-    const html = "<div class ='secim' onclick = \""+onclick+"\">"+prefix+answer+"</div>"
-    return html;
+    tahmin-=1;
+}else{
+    document.getElementById("alanId").innerHTML="Kaybettin :( Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
+}
 }
 
-// https://www.youtube.com/watch?v=XOMw3oO27kM
+const yeniOyun1 = () => {
+if(tahmin!==0){
+    let index1 = Math.floor(Math.random()*resimler.length);
+    if(x === index1){
+        index1 = (index1+1)%resimler.length;
+    }
+    const image1 = resimler[index1];
+    x = index1;
+
+    html1 = document.getElementById("img1").src=image1; 
+    if(resimler[0] === image1){
+        document.getElementById("alanId").innerHTML="Kazandın!!! Tebrik ederiz :) Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
+    } 
+    tahmin-=1;
+}else{
+    document.getElementById("alanId").innerHTML="Kaybettin :( Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
+}
+}
+const yeniOyun2 = () => {
+if(tahmin!==0){
+    let index2 = Math.floor(Math.random()*resimler.length);
+    if(x === index2){
+        index2 = (index2+1)%resimler.length;
+    }
+    const image2 = resimler[index2];
+    x = index2;    
+    html2 = document.getElementById("img2").src=image2; 
+    if(resimler[0] === image2){
+        document.getElementById("alanId").innerHTML="Kazandın!!! Tebrik ederiz :) Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
+    } 
+    tahmin-=1;
+}else{
+    document.getElementById("alanId").innerHTML="Kaybettin :( Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
+}
+}
